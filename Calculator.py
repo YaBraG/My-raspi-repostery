da6cec9b7d86c1ec72309832c67c90f3f31ce89a
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-04-26 20:13:51 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,10 +2,10 @@ Num1 = int(input("First number: "))
 Num2 = int(input("Second number: "))
 Operator=str(input("+,-,* or /: "))
 if Operator == '+':
-    print("Sum = "Num1+Num2)
+    print(Num1+Num2)
 if Operator == '-':
-    print("Substraction = "Num1-Num2)
+    print(Num1-Num2)
 if Operator == '*':
-    print("Multiplication = "Num1*Num2)
+    print(Num1*Num2)
 if Operator == '/':
-    print("Division = "Num1/Num2)
+    print(Num1/Num2)
